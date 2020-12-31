f603662d9ea424e7c9f8d2e7e4faff42c666a385
Alisa Korytova <38809145+alicekore@users.noreply.github.com>
2020-12-31 02:17:19 +0100

    Txt file creation implemented

--- Autoencoder/generator.py
+++ Autoencoder/generator.py
@@ -1,6 +1,7 @@
 from FyeldGenerator import generate_field
 import matplotlib.pyplot as plt
 import numpy as np
+np.set_printoptions(threshold=np.inf)
 
 # Helper that generates power-law power spectrum
 def Pkgen(n):
@@ -15,33 +16,31 @@ def distrib(shape):
     b = np.random.normal(loc=0, scale=1, size=shape)
     return a + 1j * b
 
-
+# Create field and save as tiff file
 def createPic(size, spec, shape=(100, 100)):
     for i in range(size):
         field = generate_field(distrib, Pkgen(spec), shape)
-        path = 'Data Set Tiff/Spectrum' + str(spec) + '/file' + str(i) + '.tiff'  # Set the path!
+        path = 'Data Set Tiff/Spectrum' + str(spec) + '/file' + str(i) + '.tiff'  # Set the correct path!
         plt.axis('off')
         plt.imshow(field, cmap='jet')
         plt.savefig(path, bbox_inches='tight', transparent=True, pad_inches=0)
         print('Done: ' + str(i))
 
-def createArray(size, spec, shape=(100, 100)):
-
+# Create field and save as txt file (one line - one field)
+def createText(size, spec, shape=(100, 100)):
+    file_name = 'Data Set/Spectrum' + str(spec) + '.txt'
+    file = open(file_name, "a+")
     for i in range(size):
         field = generate_field(distrib, Pkgen(spec), shape)
-        print(field)
+        field_text =str(field).replace('\n', '')
+        file.write(field_text + '\n')
+        print('Done:' + str(i))
+    file.close()
+
+
+
 
 
 if __name__ == "__main__":
-    # createPic(500, 1)
-    # createPic(500, 2)
-    # createPic(500, 3)
-    # createPic(500, 4)
-    # createPic(500, 5)
-    # createPic(500, 6)
-    # createPic(500, 7)
-    # createPic(500, 8)
-    # createPic(500, 9)
-    # createPic(500, 10)
-    createArray(1, 4, shape=(28, 28))
+    createText(5000, 4)
 
