597d3909d70cf23552ffd7406300bc6134d92532
Alisa Korytova <38809145+alicekore@users.noreply.github.com>
2021-01-03 19:39:42 +0100

    Reading from json added

--- Autoencoder/autoencoder.py
+++ Autoencoder/autoencoder.py
@@ -4,68 +4,86 @@ import tensorflow.keras.optimizers
 import tensorflow.keras.datasets
 import numpy
 import matplotlib.pyplot
+import os
+import codecs, json
 
-# Encoder
-x = tensorflow.keras.layers.Input(shape=(784), name="encoder_input")
+# Loads the dataset from json file and returns it as reshaped numpy array
+def prepareDataSet():
+    path = input("Enter the path of your file: ")
+    assert os.path.exists(path), "File not found at " + str(path)
+    obj_text = codecs.open(path, 'r', encoding='utf-8').read()
+    raw = json.loads(obj_text)
+    set_orig = numpy.array(raw)
+    set = numpy.reshape(set_orig, newshape=(set_orig.shape[0], numpy.prod(set_orig.shape[1:])))
+    return set
 
-encoder_dense_layer1 = tensorflow.keras.layers.Dense(units=300, name="encoder_dense_1")(x)
-encoder_activ_layer1 = tensorflow.keras.layers.LeakyReLU(name="encoder_leakyrelu_1")(encoder_dense_layer1)
 
-encoder_dense_layer2 = tensorflow.keras.layers.Dense(units=2, name="encoder_dense_2")(encoder_activ_layer1)
-encoder_output = tensorflow.keras.layers.LeakyReLU(name="encoder_output")(encoder_dense_layer2)
 
-encoder = tensorflow.keras.models.Model(x, encoder_output, name="encoder_model")
-# encoder.summary()
+def autoencoder():
+    # Encoder
+    x = tensorflow.keras.layers.Input(shape=(784), name="encoder_input")
 
-# Decoder
-decoder_input = tensorflow.keras.layers.Input(shape=(2), name="decoder_input")
+    encoder_dense_layer1 = tensorflow.keras.layers.Dense(units=300, name="encoder_dense_1")(x)
+    encoder_activ_layer1 = tensorflow.keras.layers.LeakyReLU(name="encoder_leakyrelu_1")(encoder_dense_layer1)
 
-decoder_dense_layer1 = tensorflow.keras.layers.Dense(units=300, name="decoder_dense_1")(decoder_input)
-decoder_activ_layer1 = tensorflow.keras.layers.LeakyReLU(name="decoder_leakyrelu_1")(decoder_dense_layer1)
+    encoder_dense_layer2 = tensorflow.keras.layers.Dense(units=2, name="encoder_dense_2")(encoder_activ_layer1)
+    encoder_output = tensorflow.keras.layers.LeakyReLU(name="encoder_output")(encoder_dense_layer2)
 
-decoder_dense_layer2 = tensorflow.keras.layers.Dense(units=784, name="decoder_dense_2")(decoder_activ_layer1)
-decoder_output = tensorflow.keras.layers.LeakyReLU(name="decoder_output")(decoder_dense_layer2)
+    encoder = tensorflow.keras.models.Model(x, encoder_output, name="encoder_model")
+    # encoder.summary()
 
-decoder = tensorflow.keras.models.Model(decoder_input, decoder_output, name="decoder_model")
-# decoder.summary()
+    # Decoder
+    decoder_input = tensorflow.keras.layers.Input(shape=(2), name="decoder_input")
 
-# Autoencoder
-ae_input = tensorflow.keras.layers.Input(shape=(784), name="AE_input")
-ae_encoder_output = encoder(ae_input)
-ae_decoder_output = decoder(ae_encoder_output)
+    decoder_dense_layer1 = tensorflow.keras.layers.Dense(units=300, name="decoder_dense_1")(decoder_input)
+    decoder_activ_layer1 = tensorflow.keras.layers.LeakyReLU(name="decoder_leakyrelu_1")(decoder_dense_layer1)
 
-ae = tensorflow.keras.models.Model(ae_input, ae_decoder_output, name="AE")
-# ae.summary()
+    decoder_dense_layer2 = tensorflow.keras.layers.Dense(units=784, name="decoder_dense_2")(decoder_activ_layer1)
+    decoder_output = tensorflow.keras.layers.LeakyReLU(name="decoder_output")(decoder_dense_layer2)
 
-# RMSE
-def rmse(y_true, y_predict):
-    return tensorflow.keras.backend.mean(tensorflow.keras.backend.square(y_true-y_predict))
+    decoder = tensorflow.keras.models.Model(decoder_input, decoder_output, name="decoder_model")
+    # decoder.summary()
 
-# AE Compilation
-ae.compile(loss="mse", optimizer=tensorflow.keras.optimizers.Adam(lr=0.0005))
+    # Autoencoder
+    ae_input = tensorflow.keras.layers.Input(shape=(784), name="AE_input")
+    ae_encoder_output = encoder(ae_input)
+    ae_decoder_output = decoder(ae_encoder_output)
 
-# Preparing MNIST Dataset
-(x_train_orig, y_train), (x_test_orig, y_test) = tensorflow.keras.datasets.mnist.load_data()
-x_train_orig = x_train_orig.astype("float32") / 255.0
-x_test_orig = x_test_orig.astype("float32") / 255.0
+    ae = tensorflow.keras.models.Model(ae_input, ae_decoder_output, name="AE")
+    # ae.summary()
 
-x_train = numpy.reshape(x_train_orig, newshape=(x_train_orig.shape[0], numpy.prod(x_train_orig.shape[1:])))
-x_test = numpy.reshape(x_test_orig, newshape=(x_test_orig.shape[0], numpy.prod(x_test_orig.shape[1:])))
+    # RMSE
+    def rmse(y_true, y_predict):
+        return tensorflow.keras.backend.mean(tensorflow.keras.backend.square(y_true-y_predict))
 
-# Training AE
-ae.fit(x_train, x_train, epochs=20, batch_size=256, shuffle=True, validation_data=(x_test, x_test)) # validation data <> validation! (validation set)
+    # AE Compilation
+    ae.compile(loss="mse", optimizer=tensorflow.keras.optimizers.Adam(lr=0.0005))
 
-encoded_images = encoder.predict(x_train)
-decoded_images = decoder.predict(encoded_images)
-decoded_images_orig = numpy.reshape(decoded_images, newshape=(decoded_images.shape[0], 28, 28))
+    # Preparing MNIST Dataset
+    (x_train_orig, y_train), (x_test_orig, y_test) = tensorflow.keras.datasets.mnist.load_data()
+    x_train_orig = x_train_orig.astype("float32") / 255.0
+    x_test_orig = x_test_orig.astype("float32") / 255.0
 
-num_images_to_show = 5
-for im_ind in range(num_images_to_show):
-    plot_ind = im_ind*2 + 1
-    rand_ind = numpy.random.randint(low=0, high=x_train.shape[0])
-    matplotlib.pyplot.subplot(num_images_to_show, 2, plot_ind)
-    matplotlib.pyplot.imshow(x_train_orig[rand_ind, :, :], cmap="gray")
-    matplotlib.pyplot.subplot(num_images_to_show, 2, plot_ind+1)
-    matplotlib.pyplot.imshow(decoded_images_orig[rand_ind, :, :], cmap="gray")
+    x_train = numpy.reshape(x_train_orig, newshape=(x_train_orig.shape[0], numpy.prod(x_train_orig.shape[1:])))
+    x_test = numpy.reshape(x_test_orig, newshape=(x_test_orig.shape[0], numpy.prod(x_test_orig.shape[1:])))
 
-matplotlib.pyplot.show()
+    # Training AE
+    ae.fit(x_train, x_train, epochs=20, batch_size=256, shuffle=True, validation_data=(x_test, x_test)) # validation data <> validation! (validation set)
+
+    encoded_images = encoder.predict(x_train)
+    decoded_images = decoder.predict(encoded_images)
+    decoded_images_orig = numpy.reshape(decoded_images, newshape=(decoded_images.shape[0], 28, 28))
+
+    num_images_to_show = 5
+    for im_ind in range(num_images_to_show):
+        plot_ind = im_ind*2 + 1
+        rand_ind = numpy.random.randint(low=0, high=x_train.shape[0])
+        matplotlib.pyplot.subplot(num_images_to_show, 2, plot_ind)
+        matplotlib.pyplot.imshow(x_train_orig[rand_ind, :, :], cmap="gray")
+        matplotlib.pyplot.subplot(num_images_to_show, 2, plot_ind+1)
+        matplotlib.pyplot.imshow(decoded_images_orig[rand_ind, :, :], cmap="gray")
+
+    matplotlib.pyplot.show()
+
+if __name__ == "__main__":
+    prepareDataSet()
